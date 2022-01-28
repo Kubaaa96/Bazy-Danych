0f3685dd25bf0e412a94e247b9efa90ac2f449bf
Kubaaa96 <jd.kuba96@gmail.com>
2022-01-28 12:53:46 +0100

    Updates sql and Mysql operations

--- MySQL.py
+++ MySQL.py
@@ -16,15 +16,9 @@ class MySQL_db:
 
         'CRUD part'
         self.__select()
-
-        'INSERT instuction'
-        # self.__insert()
-        # self.retrieve = "INSERT INTO `dawki` (`idd`, `ile`) VALUES (5, '3x dziennie');"
-        # self.cursor.execute(self.retrieve)
-
-        # rows = self.cursor.fetchall()
-        # for row in rows:
-        #     print(row)
+        self.__insert()
+        self.__update()
+        self.__delete()
 
 
     def __select(self):
@@ -32,6 +26,8 @@ class MySQL_db:
         for i in range(0, self.n):
             self.retrieve = "SELECT * FROM pacjenci"
             self.cursor.execute(self.retrieve)
+            result = self.cursor.fetchall()
+        self.db.commit()
         end = time.perf_counter()
         self.select_time = end - begin
         print(f"SELECT run {self.n} times in loop end after {self.select_time:0.4f} seconds")
@@ -39,22 +35,43 @@ class MySQL_db:
     def __insert(self):
         begin = time.perf_counter()
         pesel = 97090605938
-        value = 0
+        value = 1
         gabinet = 1
         for i in range(0, self.n):
-            print(value)
-            value = value + 14  # because of last key
+            value = value + 1  # because of last key
             gabinet = gabinet + 1
             pesel = pesel + 1
             self.retrieve = "INSERT INTO `doktorzy` (`idd`, `imie`, `nazwisko`, `pesel`, `telefon`, `gabinet`, `godziny`) VALUES (%s, 'Dawid', 'Mrosek', %s, '290010010', %s, 'poranne');"
             self.cursor.execute(self.retrieve, (value, pesel, gabinet))
+        self.db.commit()
         end = time.perf_counter()
         self.insert_time = end - begin
         print(f"INSERT run {self.n} times in loop end after {self.insert_time:0.4f} seconds")
 
+    def __update(self):
+        begin = time.perf_counter()
+        value = 1
+        for i in range(0, self.n):
+            value = value + 1
+            self.retrieve = "UPDATE `doktorzy` SET imie = 'Bartek' WHERE idd= %s"
+            self.cursor.execute(self.retrieve, value)
+
+        self.db.commit()
+        end = time.perf_counter()
+        self.update_time = end - begin
+        print(f"UPDATE run {self.n} times in loop end after {self.update_time:0.4f} seconds")
+
+    def __delete(self):
+        begin = time.perf_counter()
+        self.retrieve = "DELETE FROM `doktorzy`"
+        self.cursor.execute(self.retrieve)
+        self.db.commit()
+        end = time.perf_counter()
+        self.delete_time = end - begin
+        print(f"DELETE run {self.n} times in loop end after {self.delete_time:0.4f} seconds")
 
 def main():
-    test_val1 = MySQL_db(100)
+    test_val1 = MySQL_db(1)
 
 
 if __name__ == "__main__":
